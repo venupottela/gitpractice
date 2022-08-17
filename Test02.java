875d1e0334433c0a2ab511274f51dd844ab04354
venu <gvenu350@gmail.com>
2022-08-17 13:06:26 +0530

    cgange sop

--- Test02.java
+++ Test02.java
@@ -1,6 +1,6 @@
 public class Test02 {
 
 	public static void main(String[] args) {
-		System.out.println("Test2");
+		System.out.println("Test22");
 	}
 }
--- Test03.java
+++ Test03.java
@@ -1,6 +1,6 @@
 public class Test03 {
 
 	public static void main(String[] args) {
-		System.out.println("Test3");
+		System.out.println("Test33");
 	}
 }
